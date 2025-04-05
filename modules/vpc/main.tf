030222cc8a517f76aeb7435b91a2965c62e1152c
Sujit Ram Dhakal <sujit.dhakal.wrk@gmail.com>
2025-04-05 21:49:30 +0545

    feat: internet gateway, route table and route table association

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,14 +14,40 @@ resource "aws_subnet" "subnets" {
   count = length(var.subnet_cidr)
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zone.available.names[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnets"
+    Name = var.subnet_names[count.index]
   }
 }
 
 # INTERNET GATEWAY
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
 
 # ROUTE TABLE
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0" #public
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
 
 # ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "rta" {
+  count = length(var.subnet_cidr)
+  subnet_id      = aws_subnet.subnets[count.index].id
+  route_table_id = aws_route_table.rt.id
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,9 @@ variable "subnet_cidr" {
     description = "Subnet cidrs"
     type = list(string)
 }
+
+variable "subnet_names" {
+  description = "Subnet names"
+  type = list(string)
+  default = [ "PublicSubnet1", "PublicSubnet2" ]
+}
